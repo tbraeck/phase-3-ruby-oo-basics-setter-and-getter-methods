2942b135d649dc49665d32f7995ec824006abebe
Tate <tatebraeckel@gmail.com>
2023-03-28 11:32:35 -0500

    commit tree fb8ce947b6e060aa7be72ddb63baab67f7ffcc41

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
+require "pry"
+
 class Dog
 
+    attr_reader :name
+    attr_reader :breed
+
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
 end
+
+fido = Dog.new
+fido.name = "Fido"
+
+snoopy = Dog.new
+snoopy.breed = "Beagle"
+
+# binding.pry 
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
-class Person
 
-end
+require "pry"
+
+class Person
+    attr_reader :name
+    attr_reader :job
+
+        def name=(name)
+            @name = name
+        end
+    
+        def job=(job)
+            @job=job
+        end
+    end
+
+    beyonce = Person.new
+beyonce.name = "Beyonce"
+ beyonce.job = "Singer"
+
+    # binding.pry
+
+   
+
+
+
